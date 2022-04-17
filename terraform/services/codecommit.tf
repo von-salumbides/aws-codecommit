7e4c1d38da7a44c355857a5822ebdbca7c108df7
von-salumbides <vonsalumbides.devops@gmail.com>
2022-04-17 13:48:58 +0800

    Terraform: module version

--- terraform/services/codecommit.tf
+++ terraform/services/codecommit.tf
@@ -1,5 +1,5 @@
 module "main" {
-  source  = "git::https://github.com/von-salumbides/terraform-module.git//aws-codecommit"
+  source  = "git::https://github.com/von-salumbides/terraform-module.git//aws-codecommit?ref=v0.1.0"
   project = "test"
   env     = "dev"
   config  = "ecs"
